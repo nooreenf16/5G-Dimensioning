ece9c025105ae766a63ca891d4316f64dc38f433
Pranavdec27 <64825336+Pranavdec@users.noreply.github.com>
2023-02-28 17:08:05 +0530

    Delete tests directory

--- tests/mpa_sim/routes.py
+++
@@ -1,8 +0,0 @@
-def includeme(config):
-    config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
-    config.add_route('h', '/h')
-    config.add_route('hello', '/howdy')
-    config.add_route('hello_json', '/howdy.json')
-    config.add_route('hello2', '/hello2/{first}/{last}')
-    config.add_route('home2', '/home2')
